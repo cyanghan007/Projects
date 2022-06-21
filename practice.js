8e03d732f73e9013ebb7c76657d3c65512f9365d
cyanghan007 <106118567+cyanghan007@users.noreply.github.com>
2022-06-21 14:03:43 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,3 +1,11 @@
+//|| = or operater, for "or" statements only 1 side needs to be true for the code to mvoe on
+// && = and operater, for "and" statements both sides need to be true for the code to work
+// ! = Not operater, this returns true if the expressions is false
+// if and else statements can only have 1 else statement inside the {brackets}. Else if can have as many as you want
+// Operatrer precedence ((!) Not has a higher precednece than && and ||) && has a higher precedence than
+//|| but these can be altered with ()//
+
+
 let rating = 2 
 
 if (rating === 3) {
