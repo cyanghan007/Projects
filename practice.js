253dadd28bba95fc46ac340f0b244993241fdd21
cyanghan007 <106118567+cyanghan007@users.noreply.github.com>
2022-06-22 10:53:03 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -4,6 +4,7 @@
 // if and else statements can only have 1 else statement inside the {brackets}. Else if can have as many as you want
 // Operatrer precedence ((!) Not has a higher precednece than && and ||) && has a higher precedence than
 //|| but these can be altered with ()//
+
 //The switch statement is used to perform different actions based on different conditions.
 //     ex. switch (new Date().getDay()) {
 //   case 0:
@@ -18,6 +19,41 @@
 //   case 3:
 //     day = "Wednesday";
 //     break;
+
+//Objects are a collection of zero or more properties.
+//So what's a property? A property consists of a key: value pair, where the:
+//   key is a string (JS will coerce the type automatically), and the
+//   value is any JS expression (code that evaluates to a single value or thing), including other objects (yes, functions too)
+//Difference between objects and arrays - arrays use [] brackets and objects use {} bracket
+//Objecy stores key-value pairs
+    //ex. const car = {
+//           color: 'blue', //Color is the key while 'blue' is the value
+//           hp: 4000,
+//           year: 1989,
+// } 
+//console.log(car) this wil/ show the entire list above in the object
+
+//key is like an index in an array, but is has a name and is unique
+//A value is what a key refers too, and can be any datatype
+
+// Adding to the objects uses . method to add whatever value you wish
+// car.previousOwner = ['Chan', 'Yang', 'Chu', 'Sara']
+// console.log(car)
+
+/You can use object properties with conditionals, loops, etc
+// Conditional example
+// const obj = {
+// 	salutation: 'hi',
+// 	count:4
+// }
+// if (obj.salutation === "hi") {
+// 	console.log('ok');
+// }
+//  For loop example
+// for (let i = 0; i < obj.count; i++) {
+// 	console.log(i);
+// }
+
 
 let rating = 2 
 
